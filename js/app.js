736ae42d2a9207327415fcf6dc940521921e21d7
SergeyTheHuman <sergey2611.ivanov@yandex.ru>
2022-06-24 18:55:26 +0300

    added first structure & made plus/minus count on each product

--- js/app.js
+++ js/app.js
@@ -1,3 +1,31 @@
 import { isWebp } from './components/isWebp.js'
 
 isWebp()
+
+// увеличивать количество при клике на + и -   +++++
+const $products = document.querySelectorAll('.product__item')
+const $productsInCart = document.querySelectorAll('.product-cart__product')
+const $allProducts = [...$products, ...$productsInCart]
+
+function countPlus(input) {
+	let countCurrentValue = parseInt(input.value)
+	input.setAttribute('value', countCurrentValue + 1)
+}
+function countMinus(input) {
+	let countCurrentValue = parseInt(input.value)
+	if (countCurrentValue === 0) return
+	input.setAttribute('value', countCurrentValue - 1)
+}
+
+$allProducts.forEach((item) => {
+	item.addEventListener('click', (event) => {
+		const countCurrent = item.querySelector('input.count__current')
+		if (event.target.classList.contains('count__minus')) {
+			countMinus(countCurrent)
+		}
+		if (event.target.classList.contains('count__plus')) {
+			countPlus(countCurrent)
+		}
+	})
+})
+
